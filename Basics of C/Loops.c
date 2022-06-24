4f329aee81e255bdeddabd3632254cd9c854afa2
Allen-CGL <107792210+Allen-CGL@users.noreply.github.com>
2022-06-25 04:43:06 +0800

    Create Loops.c

---
+++ Basics of C/Loops.c
@@ -0,0 +1,20 @@
+//while
+
+while (boolean-expressions)
+{
+  
+}
+//and
+do 
+{
+  
+}
+while (boolean-expressions); //do the things first, then check the boolean)
+/////////////////////////////////////////////////////////////////////////////
+
+//for
+
+for (int i = 0; i < 10 ; i++) //(i = 0, i < 10, do the tings, i plus one)
+{
+  
+}
